0ee4794838e4e7d4cdb7a4f009d7804bd83dcd3f
josh <nomore2pick@gmail.com>
2019-01-09 14:49:52 -0800

    created count

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+args = process.argv.slice(2);
+function countLetters(string) {
+  var numLetter = string.join('');
+  var finalLetters = {};
+  for (var i = 0; i < numLetter.length; i++) {
+    var num = numLetter[i];
+    if (finalLetters[num]) {
+      finalLetters[num]++;
+    } else {finalLetters[num] = 1; }
+}
+return finalLetters;
+}
+console.log(countLetters(args));
